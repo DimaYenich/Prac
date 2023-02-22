e77d9fcba4b453e9f8f51bc4e5a30b745a494c3f
DimaYenich <99435046+DimaYenich@users.noreply.github.com>
2023-02-22 15:19:41 +0200

    Prac

---
+++ Day_3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Day_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("t1.dat");
+            Console.Write("Введіть текст для файлу: ");
+            sw.WriteLine(Console.ReadLine());
+            sw.Close();
+            Console.WriteLine("Файл успішно створений!");
+            Console.ReadKey();
+        }
+    }
+}
