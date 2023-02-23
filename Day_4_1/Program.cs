ce2a827980bf99726b13c6279e4f65448d4d0223
DimaYenich <99435046+DimaYenich@users.noreply.github.com>
2023-02-23 11:29:22 +0200

    Day 4

---
+++ Day_4_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Зчитується файл z4.dat");
+            StreamReader sr = new StreamReader("z4.dat");
+            string text = sr.ReadToEnd();  
+            Console.WriteLine("Вміст файлу: " + text);
+            Console.WriteLine($"Кількість символів у файлі: {text.Length}");
+            if (text.Length % 2 == 0)
+                Console.WriteLine("Кількість символів парна. Виводиться 2");
+            else
+                Console.WriteLine("Кількість символів не парна. Виводиться 1");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day_4_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Зчитується файл z4.dat");
+            StreamReader sr = new StreamReader("z4.dat");
+            string textInFile = sr.ReadToEnd();
+            int maxCode = Convert.ToByte(textInFile[0]);
+            Console.Write("\nСимвол\tЙого код\n\n");
+            for (int i = 0; i < textInFile.Length; i++)
+            {
+                if (maxCode < Convert.ToByte(textInFile[i]))
+                    maxCode = Convert.ToByte(textInFile[i]);
+
+                Console.Write(textInFile[i]);
+                Console.Write("\t"+Convert.ToByte(textInFile[i])+"\n");
+                
+            }
+            Console.WriteLine($"\nНайбільший код має \'{Convert.ToChar(maxCode)}\'" +
+                              $" з кодом {maxCode}");
+            Console.ReadKey();
+        }
+    }
+}
