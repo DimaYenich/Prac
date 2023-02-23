770aeedfd76b0c2655a1dff5747478d9875d2534
DimaYenich <99435046+DimaYenich@users.noreply.github.com>
2023-02-23 13:35:06 +0200

    Day4>all

--- Day_4_4/Program.cs
+++ Day_4_4/Program.cs
@@ -14,20 +14,27 @@ namespace Day_4_4
             Console.WriteLine("Зчитується файл z4.dat");
             StreamReader sr = new StreamReader("z4.dat");
             string textInFile = sr.ReadToEnd();
+            sr.Close();
             int maxCode = Convert.ToByte(textInFile[0]);
             Console.Write("\nСимвол\tЙого код\n\n");
+
+            string textForWrite = null;
             for (int i = 0; i < textInFile.Length; i++)
             {
                 if (maxCode < Convert.ToByte(textInFile[i]))
                     maxCode = Convert.ToByte(textInFile[i]);
-
-                Console.Write(textInFile[i]);
-                Console.Write("\t"+Convert.ToByte(textInFile[i])+"\n");
-                
+                textForWrite += textInFile[i] + "\t" + Convert.ToByte(textInFile[i]) + "\n";
             }
+            Console.Write(textForWrite);
+
             Console.WriteLine($"\nНайбільший код має \'{Convert.ToChar(maxCode)}\'" +
                               $" з кодом {maxCode}");
-            Console.ReadKey();
+            StreamWriter sw = new StreamWriter("z4-2.rez");
+
+            sw.WriteLine(textForWrite + $"\nНайбільший код має \'{Convert.ToChar(maxCode)}\'" +
+                                        $" з кодом {maxCode}");
+            sw.Close();
+            Console.ReadLine();
         }
     }
 }
